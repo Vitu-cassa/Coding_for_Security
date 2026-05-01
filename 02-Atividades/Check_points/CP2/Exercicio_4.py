5aca6b1b71506af48b1c1e02e1416eb630c07c1d
Vitu-cassa <vcassamassimo@gmail.com>
2026-04-30 21:04:06 -0300

    Conclusão do 'Exercicio_4'.

--- 02-Atividades/Check_points/CP2/Exercicio_4.py
+++ 02-Atividades/Check_points/CP2/Exercicio_4.py
@@ -83,28 +83,73 @@ def _menor_nota(lista_notas: list):
 
     return pior_aluno, menor_nota
 
+def _calculador_media(lista: list):
+    '''
+    Funcao recebe lista de nomes e notas e retorna a media das notas
+    da sala.
+    '''
+    media = 0
+    contador_notas = 0
+    somatoria = 0
+    for aluno,nota in lista:
+        somatoria += nota
+        contador_notas += 1
+    
+    media = somatoria/contador_notas
+    return media
+
+def _verificador_notas(media: float, lista: list):
+    '''
+    Função recebe a media e a lista de nome e notas e verifica os alunos com
+    nota acima da media
+    '''
+
+    for aluno, nota in lista:
+        if nota > media:
+            print(f"- {aluno}: {nota}")
+
+def _relatorio_notas(alunos_notas: list):
+
+    '''
+    Recebe as informações de notas de alunos e forata um relatorio.
+    '''
+
+    melhor_aluno, maior_nota = _maior_nota(alunos_notas)
+    pior_aluno, pior_nota = _menor_nota(alunos_notas)
+    media = _calculador_media(alunos_notas)
+
+    print("\n=== Relatório de Notas ===")
+    print(f"Maior nota: {melhor_aluno} - {maior_nota:.1f}")
+    print(f"Menor nota: {pior_aluno} - {pior_nota:.1f}")
+    print(f"Média da turma: {media:.1f}")
+    
+    print("\nAlunos acima da media:")
+    _verificador_notas(media, alunos_notas)
+
 def main():
     contador = 1
-    alunos_notas = []
+    autoteste = []
 
     _apresentacao()
-    print("Digite o nome e nota dos alunos:")
-
-    # while contador <= 5:
-
-    #     alunos_notas.append(_cadastrador_de_notas(contador))
-    #     contador += 1
-    alunos_notas = [
+    print("Autoteste:")
+    autoteste = [
     ("Carlos", 8.5),
     ("Ana", 9.2),
     ("Bruno", 6.0),
     ("Diana", 7.8),
     ("Eduardo", 4.5),
-]
-    melhor_aluno, melhor_nota = _maior_nota(alunos_notas)
-    print(melhor_aluno, melhor_nota)
-    pior_aluno, menor_nota = _menor_nota(alunos_notas)
-    print(pior_aluno, menor_nota)
+    ]
+    _relatorio_notas(autoteste)
+
+    print("\nDigite o nome e nota dos alunos para um teste manual:")
+
+    alunos_notas = []
+
+    while contador <= 5:
+
+        alunos_notas.append(_cadastrador_de_notas(contador))
+        contador += 1
+    _relatorio_notas(alunos_notas)
 
     _fim_script()
 
