65297b37bbe4078898ba50a1ac83cfb55856f37e
Vitu-cassa <vcassamassimo@gmail.com>
2026-09-19 14:40:21 -0300

    realizada organização dos diretorio. Exercicio 1 - Atualização do banco retorna erro: MySQL Connection not available

--- 02-Atividades/Check_points/Segundo_semestre/CP1/Exercicio_01.py
+++ 02-Atividades/Check_points/Segundo_semestre/CP1/Exercicio_01.py
@@ -31,6 +31,7 @@ ativos = [
 import mysql.connector
 from mysql.connector import Error
 
+
 try:
     conexao = mysql.connector.connect(
         host="localhost", user="root",
@@ -40,31 +41,80 @@ try:
         print("Conectado ao MySQL!")
 except Error as e:
     print(f"Erro de conexão: {e}")
+
 finally:
-    # Cria tabela
+
+
     cursor = conexao.cursor()
-    print("tomara que esteja criando a tabela...")
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS ativos(
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                nome VARCHAR(50),
-                ip VARCHAR(45) UNIQUE,
-                tipo VARCHAR(50),
-                criticidade ENUM('baixa', 'media', 'alta'),
-                status ENUM('ativo', 'inativo') DEFAULT 'ativo', 
-                criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                )
-    """)
 
-    # Insere os ativos
+    # Cria tabela
+    try:
+        print("tomara que esteja criando a tabela...")
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS ativos(
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    nome VARCHAR(50),
+                    ip VARCHAR(45) UNIQUE,
+                    tipo VARCHAR(50),
+                    criticidade ENUM('baixa', 'media', 'alta'),
+                    status ENUM('ativo', 'inativo') DEFAULT 'ativo', 
+                    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    )
+        """)
+    except Error as e:
+        print("Tabela não criada: {}".format(e))
 
-#    cursor.execute(
-#        "INSERT INTO ativos (nome, ip, tipo, criticidade, status) \
-#        VALUES ('teste', '1.1.1.1', 'maquininha', 'baixa', 'ativo')")
-#    conexao.commit()
-    cursor.executemany(
-       "INSERT INTO ativos (nome, ip, tipo, criticidade, status) \
-        VALUES (%s, %s, %s, %s, %s)", ativos)
-    conexao.commit()
-    if 'conexao' in locals() and conexao.is_connected():
-        conexao.close()
+    # Insere um ativo
+    print("inserindo apenas um dado...")
+    try:
+        cursor.execute(
+            "INSERT INTO ativos (nome, ip, tipo, criticidade, status) \
+            VALUES ('teste', '1.1.1.1', 'maquininha', 'baixa', 'ativo')")
+        conexao.commit()
+    except Error as e:
+        print("Ativo não cadastrado: {}".format(e))
+
+    # Insere os ativos da atividade
+    print("Inserindo ativos da lista...")
+    try:
+        cursor.executemany(
+        "INSERT INTO ativos (nome, ip, tipo, criticidade, status) \
+         VALUES (%s, %s, %s, %s, %s)", ativos
+         )
+        conexao.commit()
+        if 'conexao' in locals() and conexao.is_connected():
+            conexao.close()
+    except Error as e:
+        print("Ativo não cadastrado: {}".format(e))
+
+    # Realiza consulta na tabela por status "ativo"
+    print("Realinzando consultas...")
+
+    try:
+        cursor = conexao.cursor(dictionary=True)
+        cursor.execute(
+        "SELECT * FROM ativos WHERE status = %s", ("ativo",)
+        )
+        for row in cursor.fetchall():
+            print(f"[{row['status']}] {row['nome']} {row['ip']} {row['tipo']}")
+        conexao.commit()
+
+        if 'conexao' in locals() and conexao.is_connected():
+            conexao.close()
+    except Error as e:
+        print("Consulta não realizada: {}".format(e))
+
+    # Realiza o update de "CORE" para ativo
+    print("Atualizando servidores inativos...")
+
+    try:
+
+        print(conexao.is_connected())
+        cursor.execute(
+            "UPDATE ativos SET status = %s WHERE status = %s", ('ativo', 'inativo')
+        )
+        conexao.commit()
+        if 'conexao' in locals() and conexao.is_connected():
+            conexao.close()
+    except Error as e:
+        print("Atualização não realizada: {}".format(e))
