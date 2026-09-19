4e8dffe6d2ed44ec5012ce2bd840c55e77fdf376
Vitu-cassa <vcassamassimo@gmail.com>
2026-09-19 17:34:23 -0300

    Exercicio_2.py e duvidas.md atualizados.

--- 02-Atividades/Check_points/Segundo_semestre/CP1/Exercicio_02.py
+++ 02-Atividades/Check_points/Segundo_semestre/CP1/Exercicio_02.py
@@ -25,7 +25,6 @@ vulns = [
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError # Tentei usar isso mas não gostei
 from datetime import datetime, timezone
-from datetime import date
 from zoneinfo import ZoneInfo
 
 agora = datetime.now(ZoneInfo("America/sao_paulo")) # Tentativa de colocar uma data
@@ -38,11 +37,12 @@ vulnerabilidades = db["vulnerabilidades"]
 print(client.admin.command("ping"))
 print("Conectado ao Mongo!")
 
+
 # Insere uma vuln de teste (será inserida uma nova toda vez que o teste for executado)
 print("Inserindo uma vulnerabilidade...")
 try:
     vulnerabilidade = {
-        "cve_id": "CVE-2026-CP1", "tipo": "Teste", 
+        "cve_id": "CVE-2026-CP1", "tipo": "teste", 
         "severidade": "critica",  "corrigida": False, "timestamp": datetime.now(timezone.utc)
     }
 
@@ -62,9 +62,43 @@ except Exception as e:
 # Realiza a consulta de toda a coleção
 print("Consultando mais de uma vulnerabilidade cadastradas...")
 try:
-    for vulns in vulnerabilidades.find({"severidade": "critica"}):
-        print(vulns)
+    for vuln in vulnerabilidades.find({"severidade": "critica"}):
+        print(vuln)
+except Exception as e:
+    print("Falha na consulta multipla: {}".format(e))
+
+# Realiza a inserção de varias vulnerabilidades (lista do exercicio)
+# Serão inseridas toda vez que o teste é executado.
+print("Inserindo novas vulnerabilidades...")
+try:
+    vulnerabilidades.insert_many(vulns)
+except Exception as e:
+    print("Falha na inserção: {}".format(e))
+
+# Realiza consulta das vulnerabilidades da lista
+print("Consultando vulnerabilidades do exercicio...")
+try:
+    for vuln in vulnerabilidades.find({"cve_id": {"$regex": "CVE-2024-*"}}):
+        print(vuln)
 except Exception as e:
     print("Falha na consulta completa: {}".format(e))
 
-# Realiza a inserção de varias vulnerabilidades (lista do exercicio)
+# Realização do Update do banco (Update da CVE de teste)
+print("Realizando update...")
+try:
+    vulnerabilidades.update_many({"tipo": "Teste"}, {"$set": {"corrigida": True}})
+    vulnerabilidades.update_one({"cve_id": "CVE-2024-001"},{"$set": {"corrigida": True}})
+    for vuln in vulnerabilidades.find({}):
+            print("CVE {0[cve_id]} está: {1}".format(vuln, "Corrigida" if vuln.get("corrigida") else "Pendente"))
+except Exception as e:
+    print("Atualização não ralizada: {}".format(e))
+
+
+# Removendo "teste" da coleção
+print("Removendo vulnerabilidade de teste...")
+try:
+    vulnerabilidades.delete_many({"tipo": "Teste" })
+    for vuln in vulnerabilidades.find({}):
+        print("ID: {0[cve_id]} tipo: {0[tipo]}".format(vuln))
+except Exception as e:
+    print("Vulnerabilidade não removida: {}".format(e))
--- 02-Atividades/Check_points/Segundo_semestre/CP1/miscelanea/duvidas.md
+++ 02-Atividades/Check_points/Segundo_semestre/CP1/miscelanea/duvidas.md
@@ -1,8 +1,14 @@
-# Arquivo para registrar observações e dvidas durante a atividade
+# Arquivo para registrar observações e dúidas durante a atividade
 
 ## Exercicio 1:
 
 ## Exercicio 2:
 1. Como trabalhar com datas na hora de inserir e, principalmente, consultar um dado no mongo?
 
-2. 
+2. Como realizar comandos direto no mongoDB sem o Dbeaver?
+
+3. Gostaria de conseguir escrever melhor os códigos com uma linguagem mais pythonica.
+Se puder comentar, novamente, como condições em linha unica funcionam, pensando em semantica e estrutura, eu agradeço.
+ex. Quando atualizei tudo que teinha como "teste" no banco para "True", eu pesquisei como ficaria a saida usando o .format() com uma condicional:
+`print("CVE {0[cve_id]} está: {1}".format(vuln, "Corrigida" if vuln.get("corrigida") else "Pendente"))`
+
