39f08a05e5dc1d59abd39e3697e460b276d50eff
Vitu-cassa <vcassamassimo@gmail.com>
2026-09-20 15:23:19 -0300

    :hammer: Part: 'Exercicio_05.py' incompleto. Verificar o arquivo 'duvidas.md'.

--- 02-Atividades/Check_points/Segundo_semestre/CP1/Exercicio_05.py
+++ 02-Atividades/Check_points/Segundo_semestre/CP1/Exercicio_05.py
@@ -108,10 +108,8 @@ finally:
 
     if 'conexao' in locals() and conexao.is_connected():
         conexao.close()
-
     print("")
     print("!"* 70)
     print("Favor, verificar o arquivo de duvidas... =(")
     print("Rollback não ocorre, agora a Alice ta devendo mais de R$ 2.000,00")
     print("!"* 70)
-    
